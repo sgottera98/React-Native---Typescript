6227bbfa0812c5d66e4da730320795c091d8fd03
Bruno Sgotti <brunojf@ciandt.com>
2024-06-22 14:33:04 -0300

    useState

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+import {useState} from 'react';
+import {Platform, Text, View, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
 
 export const Home = () => {
+
+  const [newTask, setNewTask] = useState('');
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Text style={styles.title}>Hello, world!</Text>
+        <Text style={styles.title}>Tarefas</Text>
 
+        <TextInput
+          placeholderTextColor={'#555'}
+          placeholder="Adicione uma nova tarefa"
+          style={styles.input}
+          onChangeText={setNewTask}
+        />
+
+        <TouchableOpacity activeOpacity={0.7} style={styles.button} >
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.titleTasks}>Minhas tarefas</Text>
+
+        <Text style={{color: '#f1f1f1'}}>{newTask}</Text>
       </View>
     </SafeAreaView>
   );
@@ -27,5 +44,31 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: '#eba417',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  titleTasks: {
+    color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 50,
   },
 });
